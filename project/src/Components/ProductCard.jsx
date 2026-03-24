342e70cc593f6b379980ae39ff4a6090b45cd043
asfana <katheejathasfana@gmail.com>
2026-03-24 12:45:04 +0530

    navbar  updated

--- project/src/Components/ProductCard.jsx
+++ project/src/Components/ProductCard.jsx
@@ -8,7 +8,7 @@ const ProductCard = ({id,image,title,price}) => {
         <div className='card h-100 border-0 shadow-lg product-card'>
             <img src={image} alt="" className='card-img-top' style={{height:'200px', objectFit:'cover'}}/>
             <div className='card-body text-center'>
-                <h3 className='card-title'>{title}</h3>
+                <h4 className='card-title'>{title}</h4>
                 <p className='text-success fw-bold'>{price}</p>
                 <Link to={`/productDetails/${id}`}><button className='border-0 bg-dark text-light p-2 rounded fs-6'>View Details</button>
                 </Link>
